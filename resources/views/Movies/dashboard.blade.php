20cc7ab3c6a3d8fe7091f9bd53d5638f1551958f
HoncoopMilan <Honcoopmilan@gmail.com>
2024-03-19 08:52:36 +0100

    Movies dashboard

--- resources/views/Movies/dashboard.blade.php
+++ resources/views/Movies/dashboard.blade.php
@@ -10,8 +10,10 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("Popular right now!") }}
+                    <div id="Movies"></div>
                 </div>
             </div>
         </div>
     </div>
+    
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/movies', [MovieController::class, 'index']);
+Route::get('/movies', [MovieController::class, 'index'])->middleware(['auth', 'verified'])->name('movies');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
